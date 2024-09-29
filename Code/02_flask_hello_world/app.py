874cdce0794aeefc4c9f13c4ce917bb60b5b1cd5
HARISH KUMAR A <147683213+harishkumar1294@users.noreply.github.com>
2024-09-29 15:59:28 +0530

    Delete Code/02_flask_hello_world directory

--- Code/02_flask_hello_world/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World! ' + 'Harish Kumar A ' + '7376222IT147 ' + 'Dept of IT'
-
-if __name__ == '__main__':
-    app.run(debug=True)
